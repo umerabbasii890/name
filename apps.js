333f10a4e50577ec6ebb52a668c56da998bb5ae4
umerabbasii890 <140706534+umerabbasii890@users.noreply.github.com>
2023-10-13 21:47:53 +0500

    Add files via upload
    $

---
+++ apps.js
@@ -0,0 +1,55 @@
+
+
+var cars = {
+
+
+    honda : {
+
+        civic : {
+
+            reborn :{
+                  Name : civic,
+                  Model : 2017,
+                  colors : ["green","blue","pink"]
+            }
+         
+
+        },
+
+        corolla : {
+            corl2023 :{
+                Name : civic,
+                Model : 2017,
+                colors : ["green","blue","pink"]
+          }
+       
+
+
+        }
+
+
+
+
+    },
+
+    Mitsubushi :{
+
+    landcruser :{
+
+    land2023:{
+
+
+        
+    }
+
+    }
+
+
+
+    }
+
+
+
+
+
+}
